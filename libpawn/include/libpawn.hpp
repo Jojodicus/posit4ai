81fd2a3592b504a57232c2928641b0e522866712
jojodicus <jojodicus@gmail.com>
2026-04-21 23:05:02 +0200

    pure virtual

--- libpawn/include/libpawn.hpp
+++ libpawn/include/libpawn.hpp
@@ -25,35 +25,35 @@ namespace p
         const uint32_t dataWidth;
         const Datatype dataType;
 
-        virtual Number toNumber(float f) { return Number{}; }
-        virtual Number toNumber(double d) { return Number{}; }
-        virtual Number toNumber(int32_t i) { return Number{}; }
-        virtual Number toNumber(uint32_t i) { return Number{}; }
-        virtual Number toNumber(int64_t i) { return Number{}; }
-        virtual Number toNumber(uint64_t i) { return Number{}; }
-        virtual float toFloat(Number n) { return 0.0f; }
-        virtual double toDouble(Number n) { return 0.0; }
-        virtual int32_t toInt32(Number n) { return 0; }
-        virtual uint32_t toUint32(Number n) { return 0; }
-        virtual int64_t toInt64(Number n) { return 0; }
-        virtual uint64_t toUint64(Number n) { return 0; }
-        virtual std::string toString(Number n) { return ""; }
+        virtual Number toNumber(float f) = 0;
+        virtual Number toNumber(double d) = 0;
+        virtual Number toNumber(int32_t i) = 0;
+        virtual Number toNumber(uint32_t i) = 0;
+        virtual Number toNumber(int64_t i) = 0;
+        virtual Number toNumber(uint64_t i) = 0;
+        virtual float toFloat(Number n) = 0;
+        virtual double toDouble(Number n) = 0;
+        virtual int32_t toInt32(Number n) = 0;
+        virtual uint32_t toUint32(Number n) = 0;
+        virtual int64_t toInt64(Number n) = 0;
+        virtual uint64_t toUint64(Number n) = 0;
+        virtual std::string toString(Number n) = 0;
 
-        virtual Number add(Number a, Number b) { return Number{}; }
-        virtual Number sub(Number a, Number b) { return Number{}; }
-        virtual Number mul(Number a, Number b) { return Number{}; }
-        virtual Number div(Number a, Number b) { return Number{}; }
-        virtual Number neg(Number a) { return Number{}; }
-        virtual Number abs(Number a) { return Number{}; }
-        virtual Number sqrt(Number a) { return Number{}; }
-        virtual Number relu(Number a) { return Number{}; }
+        virtual Number add(Number a, Number b) = 0;
+        virtual Number sub(Number a, Number b) = 0;
+        virtual Number mul(Number a, Number b) = 0;
+        virtual Number div(Number a, Number b) = 0;
+        virtual Number neg(Number a) = 0;
+        virtual Number abs(Number a) = 0;
+        virtual Number sqrt(Number a) = 0;
+        virtual Number relu(Number a) = 0;
 
-        virtual void qaClear() {}
-        virtual void qaAdd(Number a) {}
-        virtual void qaFma(Number a, Number b) {}
-        virtual void qaFms(Number a, Number b) {}
-        virtual void qaNeg() {}
-        virtual Number qaRead() { return Number{}; }
+        virtual void qaClear() = 0;
+        virtual void qaAdd(Number a) = 0;
+        virtual void qaFma(Number a, Number b) = 0;
+        virtual void qaFms(Number a, Number b) = 0;
+        virtual void qaNeg() = 0;
+        virtual Number qaRead() = 0;
 
         virtual ~Target() = default;
     };
