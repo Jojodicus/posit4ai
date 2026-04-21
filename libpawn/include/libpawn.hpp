738c3fdac0f9831c84ffc24a032daa3dd99eb0ca
jojodicus <jojodicus@gmail.com>
2026-04-21 23:37:20 +0200

    some universal

--- libpawn/include/libpawn.hpp
+++ libpawn/include/libpawn.hpp
@@ -60,8 +60,43 @@ namespace p
 
     class UniversalTarget : public Target
     {
+        void *quire;
+
     public:
         UniversalTarget(int dataWidth, Datatype dataType = POSIT);
+
+
+        virtual Number toNumber(float f) = 0;
+        virtual Number toNumber(double d) = 0;
+        virtual Number toNumber(int32_t i) = 0;
+        virtual Number toNumber(uint32_t i) = 0;
+        virtual Number toNumber(int64_t i) = 0;
+        virtual Number toNumber(uint64_t i) = 0;
+        virtual float toFloat(Number n) = 0;
+        virtual double toDouble(Number n) = 0;
+        virtual int32_t toInt32(Number n) = 0;
+        virtual uint32_t toUint32(Number n) = 0;
+        virtual int64_t toInt64(Number n) = 0;
+        virtual uint64_t toUint64(Number n) = 0;
+        virtual std::string toString(Number n) = 0;
+
+        virtual Number add(Number a, Number b) = 0;
+        virtual Number sub(Number a, Number b) = 0;
+        virtual Number mul(Number a, Number b) = 0;
+        virtual Number div(Number a, Number b) = 0;
+        virtual Number neg(Number a) = 0;
+        virtual Number abs(Number a) = 0;
+        virtual Number sqrt(Number a) = 0;
+        virtual Number relu(Number a) = 0;
+
+        void qaClear() override;
+        virtual void qaAdd(Number a) = 0;
+        virtual void qaFma(Number a, Number b) = 0;
+        virtual void qaFms(Number a, Number b) = 0;
+        virtual void qaNeg() = 0;
+        virtual Number qaRead() = 0;
+
+        ~UniversalTarget() override;
     };
 
     class XpositTarget : public Target
--- libpawn/src/targets/universal.cpp
+++ libpawn/src/targets/universal.cpp
@@ -1,3 +1,90 @@
 #include "libpawn.hpp"
+#include "universal/number/posit/posit.hpp"
 
-p::UniversalTarget::UniversalTarget(int dataWidth, Datatype dataType) : Target(dataWidth, dataType) {}
+using posit64 = sw::universal::posit<64, 2>;
+using posit32 = sw::universal::posit<32, 2>;
+using posit16 = sw::universal::posit<16, 2>;
+using posit8 = sw::universal::posit<8, 2>;
+using quire64 = sw::universal::quire<posit64>;
+using quire32 = sw::universal::quire<posit32>;
+using quire16 = sw::universal::quire<posit16>;
+using quire8 = sw::universal::quire<posit8>;
+
+p::UniversalTarget::UniversalTarget(int dataWidth, Datatype dataType) : Target(dataWidth, dataType)
+{
+    if (dataType == POSIT)
+    {
+        switch (dataWidth)
+        {
+        case 8:
+            quire = new quire8{};
+            break;
+        case 16:
+            quire = new quire16{};
+            break;
+        case 32:
+            quire = new quire32{};
+            break;
+        case 64:
+            quire = new quire64{};
+            break;
+        default:
+            throw "Invalid data width for universal posits";
+        }
+    }
+    else // FLOAT
+    {
+        switch (dataWidth)
+        {
+        case 32:
+            quire = new float;
+            break;
+        case 64:
+            quire = new double;
+            break;
+        default:
+            throw "Invalid data width for universal floats";
+        }
+    }
+}
+
+void p::UniversalTarget::qaClear()
+{
+    if (dataType == POSIT)
+    {
+        switch (dataWidth)
+        {
+        case 8:
+            reinterpret_cast<quire8*>(quire)->clear();
+            break;
+        case 16:
+            reinterpret_cast<quire16*>(quire)->clear();
+            break;
+        case 32:
+            reinterpret_cast<quire32*>(quire)->clear();
+            break;
+        case 64:
+            reinterpret_cast<quire64*>(quire)->clear();
+            break;
+        default:
+        }
+    }
+    else // FLOAT
+    {
+        switch (dataWidth)
+        {
+        case 32:
+            *reinterpret_cast<float *>(quire) = 0;
+            break;
+        case 64:
+            *reinterpret_cast<double *>(quire) = 0;
+            break;
+        default:
+        }
+    }
+}
+
+p::UniversalTarget::~UniversalTarget()
+{
+    delete quire;
+}
