b19f76430fafb298be64c9d0005385d620b6c263
jojodicus <jojodicus@gmail.com>
2026-05-07 13:48:37 +0200

    fix clkwiz synth

--- PAWN/scripts/find_fmax.tcl
+++ PAWN/scripts/find_fmax.tcl
@@ -53,9 +53,11 @@ proc run_impl_at {freq_mhz root_dir} {
 
     # Force clk_wiz_0 to re-synthesize; without this Vivado reuses the stale
     # 100 MHz DCP and STA always reports the same WNS regardless of frequency.
-    reset_run clk_wiz_0_synth_1
-    launch_runs clk_wiz_0_synth_1 -jobs 8
-    wait_on_run clk_wiz_0_synth_1
+    if {[llength [get_runs -quiet clk_wiz_0_synth_1]] > 0} {
+        reset_run clk_wiz_0_synth_1
+        launch_runs clk_wiz_0_synth_1 -jobs 8
+        wait_on_run clk_wiz_0_synth_1
+    }
 
     reset_run synth_1
     launch_runs synth_1 -jobs 8
